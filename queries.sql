6a8a250bf293ed90b64e507db12057d0b2d93028
Nick F <ngf8@nau.edu>
2020-11-10 17:57:54 -0700

    imported properly

--- queries.sql
+++ queries.sql
@@ -16,9 +16,9 @@ where extract(year from hire_date) = 1986;
 select dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
 from dept_manager as dm
 inner join departments as d
-on dm.dept_no = d.dept_no
-left join employees as e
-on dm.emp_no = e.emp_no
+on (dm.dept_no = d.dept_no)
+inner join employees as e
+on (dm.emp_no = e.emp_no)
 order by emp_no;
 
 
